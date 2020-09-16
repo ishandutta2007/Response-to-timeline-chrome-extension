98775ab4152166fa04a4e752d0463ac755542b5b
Ishan Dutta <ishandutta2007@gmail.com>
2020-09-16 21:03:17 +0530

    pilepicker

--- popup.js
+++ popup.js
@@ -4,5 +4,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const div = document.createElement('div')
     div.textContent = `${url}: ${bg.bears[url]}`
     document.body.appendChild(div)
-  })
+  });
+
+  function handle_files(){
+    //NOt working due to security
+    let hi="hi";
+    const div = document.createElement('div')
+    div.textContent = hi
+    document.body.appendChild(div)
+  }
+
 }, false)
